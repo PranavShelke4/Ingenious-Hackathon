aee320f8bf8199eeb548f279d234d9897f2a3efb
Pranav <pranavshelke4@gmail.com>
2024-04-06 13:05:42 +0530

    Update package.json to include new dependencies and add new serviceSchema and chatRoomRoute files

---
+++ server/route/serviceRoute.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer"); // For handling file uploads
+const path = require("path");
+const Service = require("../model/serviceSchema");
+
+// Multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../uploads")); // Define the destination folder
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Define the file name
+  },
+});
+
+// Multer file filter
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Please upload only images."), false);
+  }
+};
+
+// Initialize multer with defined storage and file filter
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+// Add a new service
+router.post("/services", upload.single("image"), async (req, res) => {
+  try {
+    const { title, category, price, shortDescription, fullDescription } =
+      req.body;
+    const image = req.file.path; // Get the path of the uploaded image
+    const newService = new Service({
+      title,
+      category,
+      price,
+      shortDescription,
+      fullDescription,
+      image,
+    });
+    await newService.save();
+    res.status(201).json({ message: "Service data saved successfully" });
+  } catch (error) {
+    console.error("Error saving service data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Get all services
+router.get("/getservices", async (req, res) => {
+  try {
+    const services = await Service.find();
+    res.status(200).json(services);
+  } catch (error) {
+    console.error("Error fetching services:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
