f2fa138da944329d66c731919e1c9916fd875405
Prashant Kale <127956232+prashant-2004@users.noreply.github.com>
2024-04-06 14:15:01 +0530

    Update SearchBar.js

--- client/src/components/admin/Services/SearchBar.js
+++ client/src/components/admin/Services/SearchBar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect} from "react";
 import axios from 'axios';
 
-import React, { useState } from "react";
 import "../../../style/admin/services/SearchBar.css";
 
 function SearchBar() {
