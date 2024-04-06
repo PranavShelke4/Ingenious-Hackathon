cd59dcb4c6124c48522fd5069ec12303b9c65e4f
Prashant Kale <127956232+prashant-2004@users.noreply.github.com>
2024-04-06 13:56:24 +0530

    Updated server.js
    PORT - || 5000 added

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(require('./route/adminRoute'));
 app.use(require('./route/userRoute'));
 app.use(require('./route/serviceRoute'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, () => {
