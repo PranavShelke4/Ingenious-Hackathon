3c53d18add36ad4d916b929968f8e6530f22cb4b
Prashant Kale <127956232+prashant-2004@users.noreply.github.com>
2024-04-06 14:07:03 +0530

    Update conn.js
    MongoDB URL Added

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const DB = process.env.DATABASE;
+const DB = process.env.DATABASE || 'mongodb+srv://kaleprashant2004:MongoDB0406@services.irmjsdd.mongodb.net/?retryWrites=true&w=majority&appName=services';
 
 mongoose.connect(DB).then(() => {
     console.log('Connection Succesful');
