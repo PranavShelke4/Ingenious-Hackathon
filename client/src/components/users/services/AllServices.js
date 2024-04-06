09545ac4571a31d38aff5f85c299bdeaa82efa07
Pranav <pranavshelke4@gmail.com>
2024-04-06 14:01:19 +0530

    Refactor tab names and add new tabs in AllServices.js

--- client/src/components/users/services/AllServices.js
+++ client/src/components/users/services/AllServices.js
@@ -14,38 +14,88 @@ function AllServices() {
       <div className="AllServices">
         <div className="tab-container">
           <div
-            className={`tab ${activeTab === "Assembly" ? "active" : ""}`}
-            onClick={() => handleTabClick("Assembly")}
+            className={`tab ${activeTab === "tab1" ? "active" : ""}`}
+            onClick={() => handleTabClick("tab1")}
           >
-            Tab 1
+            Assembly
           </div>
           <div
             className={`tab ${activeTab === "tab2" ? "active" : ""}`}
             onClick={() => handleTabClick("tab2")}
           >
-            Tab 2
+            Mounting
           </div>
           <div
             className={`tab ${activeTab === "tab3" ? "active" : ""}`}
             onClick={() => handleTabClick("tab3")}
           >
-            Tab 3
+            Moving
+          </div>
+          <div
+            className={`tab ${activeTab === "tab4" ? "active" : ""}`}
+            onClick={() => handleTabClick("tab4")}
+          >
+            Cleaning
+          </div>
+          <div
+            className={`tab ${activeTab === "tab5" ? "active" : ""}`}
+            onClick={() => handleTabClick("tab5")}
+          >
+            Outdoor Help
+          </div>
+          <div
+            className={`tab ${activeTab === "tab6" ? "active" : ""}`}
+            onClick={() => handleTabClick("tab6")}
+          >
+            Home Repairs
+          </div>
+          <div
+            className={`tab ${activeTab === "tab7" ? "active" : ""}`}
+            onClick={() => handleTabClick("tab7")}
+          >
+            Painting
+          </div>
+          <div
+            className={`tab ${activeTab === "tab8" ? "active" : ""}`}
+            onClick={() => handleTabClick("tab8")}
+          >
+            Trending
           </div>
         </div>
         <div className="tab-content">
-          {activeTab === "Assembly" && (
+          {activeTab === "tab1" && (
             <div>
               <ServiceCards />
             </div>
           )}
           {activeTab === "tab2" && <div>Content for Tab 2</div>}
           {activeTab === "tab3" && <div>Content for Tab 3</div>}
+          {activeTab === "tab4" && (
+            <div>
+              <ServiceCards />
+            </div>
+          )}
+          {activeTab === "tab5" && (
+            <div>
+              <ServiceCards />
+            </div>
+          )}
+          {activeTab === "tab6" && (
+            <div>
+              <ServiceCards />
+            </div>
+          )}
+          {activeTab === "tab7" && (
+            <div>
+              <ServiceCards />
+            </div>
+          )}
+          {activeTab === "tab8" && (
+            <div>
+              <ServiceCards />
+            </div>
+          )}
         </div>
-      </div>
-      <div className="tab-content">
-        {activeTab === "Assembly" && <div>Content for Tab 1</div>}
-        {activeTab === "tab2" && <div>Content for Tab 2</div>}
-        {activeTab === "tab3" && <div>Content for Tab 3</div>}
       </div>
     </>
   );
